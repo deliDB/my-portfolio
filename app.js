b5ae726b03e14a14298f347c46280cea71814393
Deli B <106265601+deliDB@users.noreply.github.com>
2022-07-19 13:31:46 -0700

    Removes placeholder images and links
    Adds screenshots of actual projects

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 
 const { errorHandler404, globalErrorHandler } = require('./errorHandlers');
-
 const mainRoute = require('./routes');
 
 app.set('view engine', 'pug');
@@ -19,5 +18,5 @@ app.use(errorHandler404);
 app.use(globalErrorHandler);
 
 app.listen(3000, () => {
-    console.log('The application is running on localhost:3000!')
+    console.log('The application is running on localhost:3000!');
 });
